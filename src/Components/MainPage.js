d6018a64b713e1b532094fd5daa666fe5e8600b6
vanessuniq <vanessuniq@gmail.com>
2021-04-10 20:21:36 -0400

    fixed dependency of useEffect hook

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import EventList from "./EventList";
 import Header from "./Header";
 
-const BASE_API = "http://localhost:8080";
+const BASE_API = "http://localhost:8080/api/events";
 
 function MainPage() {
     //setting the state that will save all events fetched from the api
@@ -11,12 +11,12 @@ function MainPage() {
     // fetch events from api in the useEffect hook to prevent side effects
     useEffect(() => {
         axios
-            .get(`${BASE_API}/api/events`)
-            .then(response => setEvents(events.concat(response.events)))
+            .get(BASE_API)
+            .then(response => setEvents(response.data.events))
             .catch(error => {
                 console.log(error)
             })
-    }, [events]);
+    }, []);
 
     // handle the case when event list is empty
     if(events.length === 0) return (<h1>The event list is currently empty.</h1>)
